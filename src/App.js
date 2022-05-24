af62619dad47d3cc82f3995d8fd20ac59a52bacb
xjapax <jvv.joaovitor559@gmail.com>
2022-05-24 16:53:53 -0300

    DSM

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import './App.css'
+
+import Intervalo from "./components/Intervalo";
+import Media from "./components/Media"
+import Soma from "./components/Soma"
+import Sorteio from "./components/Sorteio"
+
+function App(){
+
+  return (
+    <div className="App">
+      <h1>Projeto-Redux</h1>
+      
+      <div className="linha">
+        <Intervalo></Intervalo>
+      </div>
+
+      <div className="linha">
+        <Media></Media>
+        <Soma></Soma>
+        <Sorteio></Sorteio>
+      </div>
+
+    </div>
+  )
+}
+
+export default App;
